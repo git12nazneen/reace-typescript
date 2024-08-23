f66dca91297e8fefba46d339b83e5d06347e98ee
git12nazneen <lipinazneen65@gmail.com>
2024-08-23 19:53:51 +0600

    Added useContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,41 @@
-import './App.css';
-import {Greet} from './components/Greet'
-import { Person } from './components/Person';
-import { Personlilst } from './components/Personlilst';
-import { Status } from './components/Status';
+import "./App.css";
+import { Greet } from "./components/Greet";
+import { Person } from "./components/Person";
+import { Personlilst } from "./components/Personlilst";
+import { Status } from "./components/Status";
+import { Box } from "./components/context/Box";
+import { ThemeContextProvider } from "./components/context/ThemeContext";
 function App() {
-  const personName ={
-    first : 'Sultana',
-    last: 'Lipi'
-  }
-  const personList =[
+  const personName = {
+    first: "Sultana",
+    last: "Lipi",
+  };
+  const personList = [
     {
-      first : 'Sumu',
-      last: 'dipu'
-    }, {
-      first : 'Rumu',
-      last: 'Nipu'
-    }, {
-      first : 'Lumu',
-      last: 'Kipu'
+      first: "Sumu",
+      last: "dipu",
     },
-    
-  ]
+    {
+      first: "Rumu",
+      last: "Nipu",
+    },
+    {
+      first: "Lumu",
+      last: "Kipu",
+    },
+  ];
   return (
-    <div className="App">
-      <Greet name="Nazneen" number={2} isLoggedIn={false}></Greet>
-      <Person name={personName}></Person>
-      <Personlilst names ={personList}></Personlilst>
-      <h2>Status check</h2>
-      <Status status='loading'></Status>
-    </div>
+    <ThemeContextProvider>
+      <div className="App">
+        <Box>
+          <Greet name="Nazneen" number={2} isLoggedIn={false}></Greet>
+          <Person name={personName}></Person>
+          <Personlilst names={personList}></Personlilst>
+          <h2>Status check</h2>
+          <Status status="loading"></Status>
+        </Box>
+      </div>
+    </ThemeContextProvider>
   );
 }
 
---
+++ src/components/context/theme.ts
@@ -0,0 +1,10 @@
+export const theme ={
+    primary:{
+        main:'#26374d',
+        text:'#adadad'
+    },
+    seconday:{
+        main:'#3ea3b1',
+        text:'#212121'
+    }
+}
